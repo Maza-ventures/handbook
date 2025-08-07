eae60c23e02e3deae91917900c88457003baa31b
Tambi Jalouqa <bumstyla@gmail.com>
2025-08-07 22:41:03 +0300

    added databuddy.js

---
+++ databuddy.js
@@ -0,0 +1,9 @@
+<script
+    src="https://cdn.databuddy.cc/databuddy.js"
+    data-client-id="2ewC6t0-12jEjzc0EIYnn"
+    data-enable-batching="true"
+    crossOrigin="anonymous"
+    async
+  ></script>
+
+
