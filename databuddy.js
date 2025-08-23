8e5c805c389b5f140f7988181f9927e177c231ed
Tambi Jalouqa <bumstyla@gmail.com>
2025-08-23 11:53:35 +0300

    Added a page for being globally aware in Signals vs Noise

--- databuddy.js
+++
@@ -1,9 +0,0 @@
-<script
-    src="https://cdn.databuddy.cc/databuddy.js"
-    data-client-id="2ewC6t0-12jEjzc0EIYnn"
-    data-enable-batching="true"
-    crossOrigin="anonymous"
-    async
-  ></script>
-
-
